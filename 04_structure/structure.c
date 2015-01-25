480e1bd33d6d7d99395d6699e77b1e2b937ac662
kaz_coz <keroitu@gmail.com>
2015-01-26 00:02:01 +0900

    practice of structure

--- 04_structure/structure.c
+++ 04_structure/structure.c
@@ -11,20 +11,26 @@ struct person {
 struct person person_data[3] = {
 	{"Kazumi SHIGENO", "090123456", "shigeno@example.com"},
 	{"Jim CARREY", "090987654", "jim@example.com"},
-	{"Keisuke KUWATA", "090135246", "kuwata@example.com"},
+	{"K", "090135246", "kuwata@example.com"},
 };
 
 int main(int argc, char **argv)
 {
 	//strcpy(person_data[0].name, "Kazumi SHIGENO");
 
+	FILE *fp;
+
+	fp = fopen("address.txt", "w");
+
 	int i = 0;
 	for(i = 0; i < 3; i++) {
-		printf("No.%d: \n", i + 1);
-		printf(" name: %s\n", person_data[i].name);
-		printf("phone: %s\n", person_data[i].phone);
-		printf(" mail: %s\n\n", person_data[i].mail);
+		fprintf(fp, "No.%d: \n", i + 1);
+		fprintf(fp, " name: %s\n", person_data[i].name);
+		fprintf(fp, "phone: %s\n", person_data[i].phone);
+		fprintf(fp, " mail: %s\n\n", person_data[i].mail);
 	}
+
+	fclose(fp);
 
 	return 0;
 }
