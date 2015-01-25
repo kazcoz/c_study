a13f1a96dcd292895dff4f6a8bd15b7add1345a9
kaz_coz <keroitu@gmail.com>
2015-01-25 16:43:20 +0900

    practice of structure

--- 04_structure/structure.c
+++ 04_structure/structure.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <string.h>
 
 struct person {
 	char name[32];
@@ -7,8 +8,12 @@ struct person {
 	char mail[32];
 };
 
+struct person person_data[5];
+
 int main(int argc, char **argv)
 {
+	strcpy(person_data[0].name, "Kazumi SHIGENO");
+	printf("name: %s\n", person_data[0].name);
 
 	return 0;
 }
