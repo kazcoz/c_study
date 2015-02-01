dbca34fe3799acd6b634ab600fd7c5648a468636
かず <keroitu@kcs-mba.local>
2015-02-01 16:01:32 +0900

    practice of structure

--- 04_structure/structure.c
+++ 04_structure/structure.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+void change_ul(unsigned char *c) {
+	if(*c >= 97 && *c <= 122) {
+		*c -= 32;
+	}
+}
+
 struct person {
 	char name[32];
 	char phone[11];
@@ -10,7 +16,7 @@ struct person {
 };
 
 struct person person_data[3] = {
-	{"Kazumi SHIGENO", "090123456", "shigeno@example.com"},
+	{"kazumi SHIGENO", "090123456", "shigeno@example.com"},
 	{"Jim CARREY", "090987654", "jim@example.com"},
 	{"K", "090135246", "kuwata@example.com"},
 };
@@ -19,7 +25,7 @@ int main(int argc, char **argv)
 {
 	//strcpy(person_data[0].name, "Kazumi SHIGENO");
 
-	FILE *fp;
+	/*FILE *fp;
 
 	if((fp = fopen("/root/address.txt", "w")) != NULL) {
 
@@ -34,7 +40,13 @@ int main(int argc, char **argv)
 	} else {
 		printf("ERROR: Can't open file.\n");
 		exit(1);
-	}
+	}*/
+
+
+	printf("%s\n", person_data[0].name);
+	change_ul((unsigned char *)&person_data[0].name);
+	printf("%s\n", person_data[0].name);
+
 
 
 	return 0;
